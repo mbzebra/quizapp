b5e311a84aff643374c2e5eead98291667ac828c
Marish <mbzebradev@gmail.com>
2024-04-19 06:18:02 -0700

    Updated questions

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import BookGallery from './Components/BookGallery';
+import ImageQuizComponent from './Components/ImageQuizComponent';
 
 const images = [
   { src: 'images/1.jpg', alt: 'First slide', label: 'First Slide', description: 'This is the first slide' },
   { src: 'images/2.jpg', alt: 'Second slide', label: 'Second Slide', description: 'This is the second slide' },
+  { src: 'images/3.jpg', alt: 'third slide', label: 'third Slide', description: 'This is the third slide' },
+
   // Add more images as needed
 ];
 
@@ -14,9 +16,7 @@ const Home = () => {
     <Container>
       <Row className="justify-content-md-center">
         <Col md={8} className="text-center">
-          <h1>Welcome to the Learning Portal</h1>
-            Explore various subjects and improve your knowledge. Choose from the topics above to get started!
-             <BookGallery images={images} />
+             <ImageQuizComponent images={images} lessonName="History Lesson"/>
         </Col>
       </Row>
     </Container>
